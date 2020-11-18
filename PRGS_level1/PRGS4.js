ecc024fe82ccbd2ef50f2d233e5882ba173945b0
codeamor <codeamor@github.com>
2020-11-18 10:23:07 +0900

    PRGS_2016년

---
+++ PRGS_level1/PRGS4.js
@@ -0,0 +1,33 @@
+// 체육복
+
+function solution(n, lost, reserve) {
+  let answer = n - lost.length; // 체육수업을 들을 수 있는 학생 수
+
+  // case2) 잃어버린 학생이 여별 옷을 가지고 있는 경우의 순회문
+  for (let i = 0; i < lost.length; i++) {
+    for (let j = 0; j < reserve.length; j++) {
+      if (lost[i] === reserve[j]) {
+        lost.splice(i, 1);
+        reserve.splice(j, 1);
+        answer++;
+        i--;
+        break;
+      }
+    }
+  }
+
+  // case1) 잃어버린 학생의 앞, 뒤의 학생이 여벌 옷을 가지고 있는 경우의 순회문
+  for (let i = 0; i < lost.length; i++) {
+    for (let j = 0; j < reserve.length; j++) {
+      if (reserve[j] === lost[i] - 1 || reserve[j] === lost[i] + 1) {
+        lost.splice(i, 1);
+        reserve.splice(j, 1);
+        answer++;
+        i--;
+        break;
+      }
+    }
+  }
+
+  return answer;
+}
