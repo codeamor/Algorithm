a6997db553c1ba349a822b00a108c291b4178422
codeamor <ssm2319@gmail.com>
2020-12-02 07:19:15 +0900

    PRGS_핸드폰 번호 가리기

--- PRGS_level1/PRGS019.js
+++ PRGS_level1/PRGS019.js
@@ -8,7 +8,6 @@ function solution(phone_number) {
     return stars + numbers;
 }
 
-
 // 다른 사람 풀이 1
 // 정규식
 // replace
@@ -20,8 +19,8 @@ function hide_numbers(s) {
 // \d =  0-9범위의 모든 자릿수에서
 // {4} = 4개의 자릿수를 반환하겠다.
 // regex1(?=(regex2)) => Positive Lookahead: regex1 에 regex2의 정규표현식이 뒤따르는 경우가 있을 경우 반환한다.
+// https://unlimitedpower.tistory.com/entry/정규표현식-이것이-고급이다-Positive-Negative-Lookahead-Lookbehind
 // http://stackoverflow.com/a/27545357/411615
-// https://stackoverflow.com/questions/27545202/masking-last-4-digits-in-javascript
 
 // 처음에 정규식의 Positive Lookahead가 생소하기도 하고 개념에서 preceding(followed by)이라는 말을 간과해서 왜 뒤의 4자리를 제외하고 replace 되는지 이해를 못했었다.
 // 여기서는 뒤에 4자리의 자릿수가 오는 경우에만 "*"로 대체하는 식으로 쓰였다. 그리고  Positive Lookahead는 조건식을 사용하고 버리는데, 따라서 자동으로 여과된다.
